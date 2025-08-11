f9e6809a445dd20865119c5a83011ed3390cbd4d
tuan <minhtuan034015@gmail.com>
2025-08-11 09:23:20 +0700

    Add address to order

--- 8bitstore-be/obj/Debug/net8.0/8bitstore-be.AssemblyInfo.cs
+++ 8bitstore-be/obj/Debug/net8.0/8bitstore-be.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("8bitstore-be")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddd97aa071224f60a9bd53ecbca26975a39ac60d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84d8781e513ccd2b121a0d81bfd1fc9a3ca27ab3")]
 [assembly: System.Reflection.AssemblyProductAttribute("8bitstore-be")]
 [assembly: System.Reflection.AssemblyTitleAttribute("8bitstore-be")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
